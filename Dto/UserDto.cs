f4b748c4efc3af46287fd9e646e51100323af62f
Davement <50558281+Davement@users.noreply.github.com>
2022-02-18 14:48:40 +0100

    Changed UserDto

--- Dto/UserDto.cs
+++ Dto/UserDto.cs
@@ -9,9 +9,9 @@ namespace Dtos
     public class UserDto : BaseDto
     {
         public virtual Guid Id { get; set; }
-        public virtual string Email { get; set; }
-        public virtual string Password { get; set; }
-        public virtual string FirstName { get; set; }
-        public virtual string LastName { get; set; }
+        public virtual string? Email { get; set; }
+        public virtual string? Password { get; set; }
+        public virtual string? FirstName { get; set; }
+        public virtual string? LastName { get; set; }
     }
 }
