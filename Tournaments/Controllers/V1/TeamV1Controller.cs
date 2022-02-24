a145194edfee71bb9dd611b2f6eeb16a2790cdcc
Davement <50558281+Davement@users.noreply.github.com>
2022-02-24 20:14:00 +0100

    Init TeamV1Controller

---
+++ Tournaments/Controllers/V1/TeamV1Controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tournaments.Controllers.V1
+{
+    [Route("api/v1/teams")]
+    [Produces("application/json")]
+    public class TeamV1Controller : BaseV1Controller
+    {
+
+        public TeamV1Controller()
+        {
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetTeam()
+        {
+            return Ok("Pizza");
+        }
+    }
+}
