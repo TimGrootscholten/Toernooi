61463844e1ca26fd461eb9e2565eafe1f6e75f20
Davement <50558281+Davement@users.noreply.github.com>
2022-05-12 14:18:04 +0200

    Added default PermissionGroup Everyone

---
+++ Migrations/Migrations/20220511182719_AddedDefaultPermissionGroup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Migrations.Migrations
+{
+    public partial class AddedDefaultPermissionGroup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "PermissionGroups",
+                columns: new[] { "Id", "Name", "Permissions" },
+                values: new object[] { new Guid("9cc607c1-7b93-4245-98f5-0d788cf94895"), "Everyone", "" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PermissionGroups",
+                keyColumn: "Id",
+                keyValue: new Guid("9cc607c1-7b93-4245-98f5-0d788cf94895"));
+        }
+    }
+}
